e34e78bf97c02feb07671818f34d999b9c49438b
Julian Lazaras <umbrasdracodominus@gmail.com>
2021-07-30 07:50:21 +0000

    it works with variable names equaling query/post param names

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,11 @@
+/*let sender = ...;
+tokio::spawn(move |sender|{
 
+});
+
+fn spawn<S>(value: S) -> JoinHandle<()>
+    where
+        S: Fn() -> (),
+{
+
+}*/
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+#![feature(async_closure)]
 //#![deny(warnings)]
-#![warn(rust_2018_idioms)]
+#[macro_use]
+extern crate lazy_static;
 use std::fs::File;
+use tokio::sync::mpsc::{channel, Receiver, Sender};
 
-use std::sync::{Arc, Mutex};
 use futures_util::future::join;
+use hyper::server::conn::Http;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{body::HttpBody, Body, Method, Request, Response, Server};
 use ion_shell::{
@@ -11,28 +14,24 @@ use ion_shell::{
 };
 use small::string::String as string;
 use std::collections::HashMap;
-use std::rc::Rc;
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
 
-static INDEX1: &[u8] = b"The 1st service!";
-static INDEX2: &[u8] = b"The 2nd service!";
-
-async fn read_post(
-    request: &mut Request<Body>,
-) -> HashMap<small::string::String, Value<Rc<Function>>> {
+async fn read_post(request: &mut Request<Body>) -> HashMap<String, String> {
     let body = String::from_utf8(request.data().await.unwrap().unwrap().to_vec()).unwrap();
     let args: Vec<&str> = body.split("&").collect();
     let mut ret = HashMap::new();
     for arg in args.iter() {
         let parts: Vec<&str> = arg.split("=").collect();
         ret.insert(
-            string::from_string(parts.get(0).unwrap().to_string()),
-            Value::Str(string::from_string(parts.get(1).unwrap().to_string())),
+            parts.get(0).unwrap().to_string(),
+            parts.get(1).unwrap().to_string(),
         );
     }
     ret
 }
 
-fn read_get(request: &Request<Body>) -> HashMap<small::string::String, Value<Rc<Function>>> {
+fn read_get(request: &Request<Body>) -> HashMap<String, String> {
     match request.uri().query() {
         Some(param_list) => {
             let args: Vec<&str> = param_list.split("&").collect();
@@ -40,8 +39,8 @@ fn read_get(request: &Request<Body>) -> HashMap<small::string::String, Value<Rc<
             for arg in args.iter() {
                 let parts: Vec<&str> = arg.split("=").collect();
                 ret.insert(
-                    string::from_string(parts.get(0).unwrap().to_string()),
-                    Value::Str(string::from_string(parts.get(1).unwrap().to_string())),
+                    parts.get(0).unwrap().to_string(),
+                    parts.get(1).unwrap().to_string(),
                 );
             }
             ret
@@ -50,50 +49,68 @@ fn read_get(request: &Request<Body>) -> HashMap<small::string::String, Value<Rc<
     }
 }
 
-async fn index1(mut request: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    
-    let mut shell = Shell::new();
-    for (key, value) in read_get(&request).into_iter() {
-        shell.variables_mut().set(&key, value);
-    }
-
-    if request.method() == Method::POST {
-        for (key, value) in read_post(&mut request).await.into_iter() {
-            shell.variables_mut().set(&key, value);
-        }
-    }
-    if let Ok(file) = File::open("examples/example.ion") {
-        if let Err(why) = shell.execute_command(file) {
-            println!("ERROR: my-application: error in config file: {}", why);
-        }
-    }
-
-
-    Ok(Response::new(Body::from(INDEX1)))
-}
-
-async fn index2(_: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    Ok(Response::new(Body::from(INDEX2)))
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     //pretty_env_logger::init();
 
-    let addr1 = ([127, 0, 0, 1], 1337).into();
-    let addr2 = ([127, 0, 0, 1], 1338).into();
+    let (s, mut r): (
+        Sender<(String, HashMap<String, String>, HashMap<String, String>)>,
+        Receiver<(String, HashMap<String, String>, HashMap<String, String>)>,
+    ) = channel(16);
 
-    let srv1 = Server::bind(&addr1).serve(make_service_fn(|_| async {
-        Ok::<_, hyper::Error>(service_fn(index1))
-    }));
+    tokio::spawn(async move {
+        while let Some((path, get, post)) = r.recv().await {
+            std::thread::spawn(move ||{
+                let mut shell = Shell::new();
+                for (key, value) in get.into_iter() {
+                    shell.variables_mut().set(&key, Value::Str(string::from_string(value)));
+                }
+                for (key, value) in post.into_iter() {
+                    shell.variables_mut().set(&key, Value::Str(string::from_string(value)));
+                }
+    
+                if let Ok(file) = File::open("config.ion") {
+                    if let Err(why) = shell.execute_command(file) {
+                        println!("ERROR: my-application: error in config file: {}", why);
+                    }
+                }
+            });
+        }
+    });
 
-    let srv2 = Server::bind(&addr2).serve(make_service_fn(|_| async {
-        Ok::<_, hyper::Error>(service_fn(index2))
-    }));
+    let addr: SocketAddr = ([127, 0, 0, 1], 8080).into();
 
-    println!("Listening on http://{} and http://{}", addr1, addr2);
+    let tcp_listener = TcpListener::bind(addr).await?;
+    loop {
+        let (tcp_stream, _) = tcp_listener.accept().await?;
+        //tokio::task::spawn(async move {
+        let sender = s.clone();
+        if let Err(http_err) = Http::new()
+            .http1_only(true)
+            .http1_keep_alive(true)
+            .serve_connection(
+                tcp_stream,
+                service_fn(move |mut request| {
+                    let send = sender.clone();
+                    async move {
+                        let getparams = read_get(&request);
+                        let postparams = if request.method() == Method::POST {
+                            read_post(&mut request).await
+                        } else {
+                            HashMap::new()
+                        };
+                        send.send((request.uri().path().to_string(), getparams, postparams))
+                            .await
+                            .unwrap();
 
-    let _ret = join(srv1, srv2).await;
-
-    Ok(())
+                        Ok::<_, hyper::Error>(Response::new(Body::from("It worked")))
+                    }
+                }),
+            )
+            .await
+        {
+            eprintln!("Error while serving HTTP connection: {}", http_err);
+        }
+        //});
+    }
 }
